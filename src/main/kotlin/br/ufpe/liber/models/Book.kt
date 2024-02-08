f0de7fb783d7fd15f63a43d5ce6f408596b6d102
Marcos Pereira <marcos.silva@gmail.com>
2024-02-08 09:41:50 -0500

    fix: remove new book/year endopoint

--- src/main/kotlin/br/ufpe/liber/models/Book.kt
+++ src/main/kotlin/br/ufpe/liber/models/Book.kt
@@ -20,7 +20,6 @@ data class Book(
         }
     }
 
-    fun firstPage(year: Int): Optional<Page> = nextPage(0, year)
     fun page(page: Int, year: Int): Optional<Page> = Optional.ofNullable(pagesMap[Key(page, year)])
 
     fun nextPage(page: Int, year: Int): Optional<Page> =
